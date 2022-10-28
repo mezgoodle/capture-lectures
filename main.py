d04261800f8ede421779cfb7bf785e17a9d122fe
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-28 16:04:10 +0300

    Some ready script

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import numpy as np
 import time
 from datetime import date
 
-today = date.today()
-date_format = today.strftime("%d-%m-%Y")
-
-subject_name = 'Economics'
-
+TODAY = date.today()
+DATE_FORMAT = TODAY.strftime("%d-%m-%Y")
+REGION = (372, 84, 818, 254)
+SLEEP_SECONDS = 2
+SUBJECT_NAME = 'Economics'
 counter = 0
 
 while True:
-	time.sleep(2)
-	screenshot_name = f'{subject_name}_{date_format}_{counter}.png'
+	time.sleep(SLEEP_SECONDS)
+	screenshot_name = f'{SUBJECT_NAME}_{DATE_FORMAT}_{counter}.png'
 	previous = cv2.imread(screenshot_name)
 	if previous is not None:
-		current_screen = pyautogui.screenshot()
-		current = np.array(current_screen)
+		current_screen = pyautogui.screenshot(region=REGION)
+		current = cv2.cvtColor(np.array(current_screen), cv2.COLOR_RGB2BGR)
 		difference = cv2.subtract(previous, current)
 		b, g, r = cv2.split(difference)
 		if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
 			pass
 		else:
 			counter += 1
-			screenshot_name = f'{subject_name}_{date_format}_{counter}.png'
+			print('New photo')
+			screenshot_name = f'{SUBJECT_NAME}_{DATE_FORMAT}_{counter}.png'
 			current_screen.save(screenshot_name)
 	else:
-		_ = pyautogui.screenshot(screenshot_name)
+		print('The first photo')
+		_ = pyautogui.screenshot(screenshot_name, region=REGION)
